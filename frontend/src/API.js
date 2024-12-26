d14271b6c9bb5e02559d5dbdb02006c8cddc9bc2
Ahtisham Ul Haq <77314350+ahtisham774@users.noreply.github.com>
2024-12-26 11:51:22 +0500

    first commit

---
+++ frontend/src/API.js
@@ -0,0 +1,5 @@
+// const URI = 'http://localhost:8080';
+//const URI = 'https://rogue-mewing-krill.glitch.me';
+const URI = 'https://tickit-app-ejctd3d5a5gdcjej.canadacentral-01.azurewebsites.net'
+export const BASE_URL = URI ;
+export const AUTH = `${BASE_URL}/auth`;
